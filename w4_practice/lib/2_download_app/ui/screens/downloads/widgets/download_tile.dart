8d76a671265fc2b61f0465814b8dbcaeb3a012d8
OengGechty <gechtyoeng@gmail.com>
2026-02-22 08:12:19 +0700

    TASK002-Downloader update-download tile with controller

--- w4_practice/lib/2_download_app/ui/screens/downloads/widgets/download_tile.dart
+++ w4_practice/lib/2_download_app/ui/screens/downloads/widgets/download_tile.dart
@@ -1,18 +1,45 @@
 import 'package:flutter/material.dart';
- 
+import 'package:w4_practice/2_download_app/ui/theme/theme.dart';
 import 'download_controler.dart';
 
 class DownloadTile extends StatelessWidget {
   const DownloadTile({super.key, required this.controller});
 
   final DownloadController controller;
- 
- // TODO
+
+  // Get the icon based on download status
+  IconData _downloadIcon(DownloadStatus status) {
+    switch (status) {
+      case DownloadStatus.notDownloaded:
+        return Icons.download;
+      case DownloadStatus.downloading:
+        return Icons.downloading_outlined;
+      case DownloadStatus.downloaded:
+        return Icons.folder;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
-     
-    // TODO
+    String title = controller.ressource.name;
+    String subTitle = controller.status == DownloadStatus.downloading || controller.status == DownloadStatus.downloaded
+        ? "${controller.progress * 100} % completed - ${controller.downLoadSize} of ${controller.ressource.size}"
+        : "";
+    return ListenableBuilder(
+      listenable: controller,
+      builder: (context, widget) {
+        return Container(
+          decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(12)),
+          child: ListTile(
+            title: Text(title, style: TextStyle(color: AppColors.text)),
+            subtitle: Text(subTitle, style: TextStyle(color: AppColors.textLight)),
+            trailing: IconButton(
+              onPressed: () => controller.startDownload(),
+              icon: Icon(_downloadIcon(controller.status), color: AppColors.iconNormal),
+            ),
+          ),
+        );
+      },
+    );
   }
 }
